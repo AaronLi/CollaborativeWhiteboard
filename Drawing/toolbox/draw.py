4b5ebb31a01e65caadb3ce139c6963d341b254fd
DanielEDuan <daniel.duan01@gmail.com>
2021-02-06 21:27:29 -0500

    Added lines to draw and erase

--- Drawing/toolbox/draw.py
+++ Drawing/toolbox/draw.py
@@ -2,20 +2,20 @@ import cv2
 
 
 class Write:
-    #def __init__(self):
-        #self.size = 1
-        #self.color = (0,0,255)
+    def __init__(self):
+        self.prev_coord = (0,0)
 
     @staticmethod
-    def draw(img,event,x,y,size,color):
+    def draw(self,img,event,x,y,size,color):
         if event == cv2.EVENT_LBUTTONDOWN:
             #drawmode = DrawingMode.DRAWING
             cv2.circle(img, (x, y), size, color, -1)
+            self.prev_coord = (x,y)
 
         elif event == cv2.EVENT_MOUSEMOVE:
             #if drawmode == DrawingMode.DRAWING:
             cv2.circle(img, (x, y), size, color, -1)
-
-
+            cv2.line(img, (x, y), self.prev_coord, color, size)
+            self.prev_coord = (x, y)
         #elif event == cv2.EVENT_LBUTTONUP:
             #OnHand.set_mode(None)
--- Drawing/toolbox/erase.py
+++ Drawing/toolbox/erase.py
@@ -1,23 +1,19 @@
 import cv2
 
 class Erase:
-    #def __init__(self):
-        #self.size = 20
-        #self.color = (0, 0, 0)
-        #self.drawmode = None
+    def __init__(self):
+        self.prev_coord(0,0)
 
-    #def sizeUp(self):
-        #self.size+=5
-
-    #def sizeDown(self):
-        #self.size-=5
     @staticmethod
-    def draw(img,event,x,y,size,color):
+    def draw(self,img,event,x,y,size,color):
 
         if event == cv2.EVENT_LBUTTONDOWN:
             #self.drawmode = DrawingMode.DRAWING
             cv2.circle(img, (x, y), size, (0,0,0), -1)
+            self.prev_coord = (x,y)
 
         elif event == cv2.EVENT_MOUSEMOVE:
             #if self.drawmode == DrawingMode.DRAWING:
             cv2.circle(img, (x, y), size, (0,0,0), -1)
+            cv2.line(img, (x, y), self.prev_coord, (0,0,0), size)
+            self.prev_coord=(x,y)
